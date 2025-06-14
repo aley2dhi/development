fff1bdbeef1828d3dce4668b17d3e4e1082a301a
aley2dhi <aleyadhi@hotmail.com>
2025-06-14 20:12:06 +0300

    commit #7

--- rufaga_annoor_project/settings.py
+++ rufaga_annoor_project/settings.py
@@ -20,9 +20,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['rofaga-annoor.onrender.com','*']
 
-#CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = ['https://rofaga-annoor.onrender.com']
 
 # Application definition
 
